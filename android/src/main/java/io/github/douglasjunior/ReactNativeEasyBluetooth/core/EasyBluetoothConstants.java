4d306f0f57e3d427c7d51f08de4d7b7a6d2e1bbe
Douglas Nassif Roma Junior <douglas@appmoove.com.br>
2017-05-25 09:03:30 -0300

    First commit

---
+++ android/src/main/java/io/github/douglasjunior/ReactNativeEasyBluetooth/core/EasyBluetoothConstants.java
@@ -0,0 +1,17 @@
+
+package io.github.douglasjunior.ReactNativeEasyBluetooth.core;
+
+/**
+ * Created by dougl on 09/04/2017.
+ */
+
+public final class EasyBluetoothConstants {
+    private EasyBluetoothConstants() {
+    }
+
+    public static final String EVENT_DEVICE_FOUND = "EVENT_DEVICE_FOUND";
+    public static final String EVENT_DATA_READ = "EVENT_DATA_READ";
+    public static final String EVENT_STATUS_CHANGE = "EVENT_STATUS_CHANGE";
+    public static final String EVENT_DEVICE_NAME = "EVENT_DEVICE_NAME";
+
+}
