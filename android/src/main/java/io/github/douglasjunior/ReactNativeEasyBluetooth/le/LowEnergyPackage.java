b954ee817f42244fe99cd85197f6a42ffa8ea431
Douglas Nassif Roma Junior <douglas@appmoove.com.br>
2017-05-30 18:00:36 -0300

    Implemented LowEnergyModule

--- android/src/main/java/io/github/douglasjunior/ReactNativeEasyBluetooth/le/LowEnergyPackage.java
+++ android/src/main/java/io/github/douglasjunior/ReactNativeEasyBluetooth/le/LowEnergyPackage.java
@@ -30,13 +30,10 @@ import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.github.douglasjunior.bluetoothlowenergylibrary.BluetoothLeService;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import io.github.douglasjunior.ReactNativeEasyBluetooth.core.CoreModule;
 
 /**
  * Created by douglas on 24/05/17.
@@ -47,7 +44,7 @@ public class LowEnergyPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
-        modules.add(new CoreModule(reactContext, "EasyBluetoothLE", BluetoothLeService.class));
+        modules.add(new LowEnergyModule(reactContext));
         return modules;
     }
 
